fec53d4b6f7eb89f3938ac0bd50cace04ddd23f2
Robert Dragan <robertnote23@gmail.com>
2021-02-25 20:01:47 +0100

    finish

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -1,6 +1,9 @@
 const authService = require('./authService');
 
 const generateToken = (req, res) => {
+    if (req.body.email === undefined) {
+        return res.sendStatus(400);
+    }
     const token = authService.generateAccessToken({email: req.body.email})
     const response = {
         "accessToken": token
